a1958cbc19035b8e99275eaae6eecfb9f0aa0e4b
Ian Claxton <ian_claxton@hotmail.com>
2018-02-12 09:29:51 +0000

    Downgraded JSON.Net to meet nuget dependency requirements

--- FitAPI.Functions/FitAPIFunctions/mergeClasses/_mergeClass.cs
+++ FitAPI.Functions/FitAPIFunctions/mergeClasses/_mergeClass.cs
@@ -1,29 +1,26 @@
-﻿using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
 using Dapper;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using System.Threading.Tasks;
 using FitAPIFunctions.Schema;
-using System.Collections.Generic;
 using System.Reflection;
 using System;
 
 namespace FitAPIFunctions
 {
-    public static class mergeExercise3
+    public static class MergeExercise3
     {
         
 
         [FunctionName("mergeExercise3")]
-        public async static Task Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "merge/Exercise3")]HttpRequestMessage req, TraceWriter log)
+        public async static Task<HttpStatusCode> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "merge/Exercise3")]HttpRequestMessage req, TraceWriter log)
         {
             StreamContent content = (StreamContent)req.Content;
             var postData = await content.ReadAsStringAsync();
@@ -34,8 +31,9 @@ namespace FitAPIFunctions
             var sqlConnectionString =
                 ConfigurationManager
                    .ConnectionStrings["FitDB_conn"].ConnectionString;
-            string JSON = "Error occurred";
+
             HttpStatusCode statusCode = HttpStatusCode.OK;
+
             try
             {
                 using (SqlConnection conn = new SqlConnection(sqlConnectionString))
@@ -60,7 +58,7 @@ namespace FitAPIFunctions
                 }
 
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 log.Error("C# HTTP trigger function encountered an error ", ex);
                 statusCode = HttpStatusCode.InternalServerError;
@@ -68,7 +66,7 @@ namespace FitAPIFunctions
             }
             //Always return to not leave the client hanging
             //return req.CreateResponse(statusCode, JSON);
-            return;
+            return statusCode;
         }
     }
 }
