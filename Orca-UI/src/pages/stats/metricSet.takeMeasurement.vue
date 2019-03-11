825689f157faf34a7779cf2411c5dd7e05a11ce8
Mark <mark.stacey@aphelion.bi>
2019-03-11 21:58:04 +0000

    Tweaked to work

--- Orca-UI/src/pages/stats/metricSet.takeMeasurement.vue
+++ Orca-UI/src/pages/stats/metricSet.takeMeasurement.vue
@@ -60,21 +60,26 @@ export default {
     let flagChanges = false
     let control = this.$refs.control
     let data = this.$refs.data
-    flagChanges = typeof _.first(control.metricDetailList, newData => {
+    flagChanges = typeof _.find(control.metricDetailList, newData => {
       let dataItem = data.LatestValues[newData.MetricDetailID]
       // No need to maintain a working copy of the original values, as the Latest is where we retrieved them from anyway
-      if (typeof dataItem === 'undefined') return true
+      if (typeof dataItem === 'undefined') {
+        if (newData.DecimalValue !== 0 ||
+        newData.IntegerValue !== 0 ||
+        newData.FloatValue !== 0) return true // A value has been set
+        return false // Nothing has been set
+      }
       if (newData.DecimalValue !== dataItem.DecimalValue ||
         newData.IntegerValue !== dataItem.IntegerValue ||
-        newData.FloatValue !== dataItem.FloatValue ||
-        newData.BoolValue !== dataItem.BoolValue
+        newData.FloatValue !== dataItem.FloatValue // ||
+        // newData.BoolValue !== dataItem.BoolValue /// TODO:
       ) return true
-      return false
+      return false // Data is the same
     }) !== 'undefined'
     if (!flagChanges) {
       next()
     } else {
-      uiMixin(this.saveData)
+      this.uiSaveAction(next, this.saveData)
     }
   }
 }
